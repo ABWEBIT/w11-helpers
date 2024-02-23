9a20d7b3764576592e2c8198677a9c2136ebe171
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2024-02-23 15:20:22 +0200

    Update W11-version-viewer.bat

--- W11-version-viewer.bat
+++ W11-version-viewer.bat
@@ -1,4 +1,9 @@
 @echo off
-winver
+title System Info
+powershell $info = Get-ComputerInfo -Property OSName,OSDisplayVersion,OsBuildNumber; ^
+Write-Host "Name: "($info).OSName; ^
+Write-Host "Version: "($info).OSDisplayVersion; ^
+Write-Host "Build: "($info).OsBuildNumber;
 
+pause > nul
 exit
