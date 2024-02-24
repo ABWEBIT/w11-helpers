b4466702df30e782ab5f0aca8b88d6cb0c61d748
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2024-02-24 08:53:36 +0200

    Add files via upload

---
+++ W11-nvidia-cleaner.bat
@@ -0,0 +1,16 @@
+@echo off
+title NVIDIA Cleaner
+
+powershell -NoProfile -Command ^
+  $Installer = $env:PROGRAMFILES+'\NVIDIA Corporation\Installer2'; ^
+  $approval = Read-Host -Prompt 'Clear NVIDIA Installer - y / n'; ^
+  if ((Test-Path -Path $Installer) -and ($approval -eq 'y')){Remove-Item $Installer"\*" -Force -Recurse -ErrorAction SilentlyContinue}; ^
+  Remove-Variable -Name Installer, approval
+
+powershell -NoProfile -Command ^
+  $DXCache = $env:USERPROFILE+'\AppData\LocalLow\NVIDIA\PerDriverVersion\DXCache'; ^
+  $approval = Read-Host -Prompt 'Clear NVIDIA DXCache - y / n'; ^
+  if ((Test-Path -Path $DXCache) -and ($approval -eq 'y')){Remove-Item $DXCache"\*" -Force -Recurse -ErrorAction SilentlyContinue}; ^
+  Remove-Variable -Name DXCache, approval
+
+exit
