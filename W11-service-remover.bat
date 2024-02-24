063ce6a1b1c45433b86f8ce8713919f0375dc78c
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2024-02-24 07:53:25 +0200

    Update W11-service-remover.bat

--- W11-service-remover.bat
+++ W11-service-remover.bat
@@ -1,5 +1,10 @@
-net stop ServiceName
-sc delete ServiceName
+@echo off
+title Service Remover
+setlocal
+
+SET /P SeriveName="Enter Service Name: "
+net stop %SeriveName%
+sc delete %SeriveName%
 
 pause > nul
 exit
