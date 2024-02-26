f63285391eaa412853e0cfc40153007f9425e657
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2024-02-26 21:27:05 +0200

    Update W11-registry-modifications.bat

--- W11-registry-modifications.bat
+++ W11-registry-modifications.bat
@@ -7,6 +7,9 @@ reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\Inpr
 :: Remove Bing From the Start Menu
 reg add "HKCU\Software\Policies\Microsoft\Windows\Explorer" /v DisableSearchBoxSuggestions /t REG_DWORD /d 1 /f
 
+:: Select This PC instead of Quick Access in File Explorer
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v LaunchTo /t REG_DWORD /d 1 /f
+
 taskkill /F /IM explorer.exe & start explorer
 
 exit
