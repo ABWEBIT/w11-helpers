e5d8e869dac0857fd12e7470207e8780a8f8974c
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2024-02-26 21:11:22 +0200

    Update and rename W11-system-more-options.bat to W11-registry-modifications.bat

--- W11-registry-modifications.bat
+++ W11-registry-modifications.bat
@@ -1,8 +1,12 @@
 @echo off
-title System More Options
+title Registry Modifications
 
 :: Show More Options on Right Click
 reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
+:: Remove Bing From the Start Menu
+reg add "HKCU\Software\Policies\Microsoft\Windows\Explorer" /v DisableSearchBoxSuggestions /t REG_DWORD /d 1 /f
+
 taskkill /F /IM explorer.exe & start explorer
 
 exit
