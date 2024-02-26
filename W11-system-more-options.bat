8adf4b6cc9274860955addc33c3aa60ae91b48a7
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2024-02-26 21:07:49 +0200

    Update W11-system-more-options.bat

--- W11-system-more-options.bat
+++ W11-system-more-options.bat
@@ -2,7 +2,7 @@
 title System More Options
 
 :: Show More Options on Right Click
-reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 taskkill /F /IM explorer.exe & start explorer
 
 exit
