f900378ce843aafec9ba0da23d4f3d98a467464d
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2024-02-27 07:24:21 +0200

    Update and rename W11-registry-modifications.bat to W11-registry-tweaks.bat

--- W11-registry-tweaks.bat
+++ W11-registry-tweaks.bat
@@ -1,5 +1,5 @@
 @echo off
-title Registry Modifications
+title Registry Tweaks
 
 :: Show More Options on Right Click
 reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
