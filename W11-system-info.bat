04e6c92a170a99a62d603546c9a02b6fe504f8f7
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2024-02-27 17:16:13 +0200

    Update W11-system-info.bat

--- W11-system-info.bat
+++ W11-system-info.bat
@@ -1,10 +1,22 @@
 @echo off
 title System Info
 
-powershell $info = Get-ComputerInfo -Property OSName,OSDisplayVersion,OsBuildNumber; ^
-Write-Host "Name: "($info).OSName; ^
-Write-Host "Version: "($info).OSDisplayVersion; ^
-Write-Host "Build: "($info).OsBuildNumber;
+powershell ^
+$sys = Get-ComputerInfo -Property OSName,BiosSMBIOSBIOSVersion,OSDisplayVersion,OsBuildNumber,CsModel,CsManufacturer,CsSystemFamily,CsProcessors,CsPhyicallyInstalledMemory,OsTotalVisibleMemorySize; ^
+$gpu = Get-WmiObject win32_VideoController; ^
+Write-Host "CPU: "$sys.CsProcessors.Name; ^
+Write-Host "Physical Cores: "$sys.CsProcessors.NumberOfCores; ^
+Write-Host "Logical Cores: "$sys.CsProcessors.NumberOfLogicalProcessors; ^
+Write-Host "GPU: "("($gpu | ForEach-Object { $_.Name }) -join ' - '"); ^
+Write-Host "RAM Installed: "([math]::round($sys.CsPhyicallyInstalledMemory / 1Mb))" Gb"; ^
+Write-Host "RAM Available: "([math]::round($sys.OsTotalVisibleMemorySize / 1Mb))" Gb"; ^
+Write-Host "MTB / Model: "$sys.CsModel; ^
+Write-Host "Family: "$sys.CsSystemFamily; ^
+Write-Host "Manufacturer: "$sys.CsManufacturer; ^
+Write-Host "BIOS Version: "$sys.BiosSMBIOSBIOSVersion; ^
+Write-Host "OS Name: "$sys.OSName; ^
+Write-Host "OS Version: "$sys.OSDisplayVersion; ^
+Write-Host "OS Build: "$sys.OsBuildNumber;
 
 pause > nul
 exit
