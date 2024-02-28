54229bb70eb36377bf3270d0e9483b988309f331
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2024-02-28 09:56:55 +0200

    Update W11-registry-modifications.bat

--- W11-registry-modifications.bat
+++ W11-registry-modifications.bat
@@ -4,5 +4,8 @@ title Registry Modifications
 :: Disable Delivery Optimization
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\DoSvc" /v Start /t REG_DWORD /d 4 /f
 
+taskkill /F /IM explorer.exe
+start explorer.exe
+
 pause > nul
 exit
