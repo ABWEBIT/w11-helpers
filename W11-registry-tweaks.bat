a9cefada712422712f1daee798bc5712ee8e776a
ABWEBIT <108275652+ABWEBIT@users.noreply.github.com>
2024-02-28 10:49:23 +0200

    Update W11-registry-tweaks.bat

--- W11-registry-tweaks.bat
+++ W11-registry-tweaks.bat
@@ -1,10 +1,10 @@
 @echo off
 title Registry Tweaks
 
-:: Restore Old Right-click Context Menu
+:: Restore old Right-click context menu
 reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
 
-:: Remove Bing From the Start Menu
+:: Remove Bing from the start menu
 reg add "HKCU\Software\Policies\Microsoft\Windows\Explorer" /v DisableSearchBoxSuggestions /t REG_DWORD /d 1 /f
 
 :: Open File Explorer to: This PC
@@ -13,7 +13,7 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v La
 :: Show File Extensions
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
 
-:: Lowering Menu Show Delay 400 -> 100
+:: Lowering menu show delay 400 -> 100
 reg add "HKCU\Control Panel\Desktop" /v MenuShowDelay /t REG_SZ /d 100 /f
 
 :: Disable Advertising ID for Personalized Ads
